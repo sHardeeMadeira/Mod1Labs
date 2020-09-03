523a08c8833054f56acb931cb890eb9085271d4d
sHardeeMadeira <70650894+sHardeeMadeira@users.noreply.github.com>
2020-09-03 12:10:54 -0400

    Update Main.java

--- src/madeira/cs/Main.java
+++ src/madeira/cs/Main.java
@@ -1,8 +1,37 @@
 package madeira.cs;
 
-public class Main {
+/**
+* Author: Susanna Hardee
+* Date: 9/3/2020
+**/
+public class Lab1 {
+    public static void main(string[] args) {
+        /*
+        Instructions for use 
+        ------
+        Write a program that does the following
+        I. Declare and instantiate three strings
+            a. Print each string separately
+        II. Re-assign a new value to the strings you declared
+            a. Print the new strings separately
+        III. Visit the link in the README.md section
+            a. Find three string methods you like
+            b. Use them however you so choose
+       */
+        
+        public class main {
+            public static void main (String [] args) {
+                String mystring = "My first string";
+                
+       
+               
+                    
+                
+                
+         
+    
 
-    public static void main(String[] args) {
+    
 
 
 
